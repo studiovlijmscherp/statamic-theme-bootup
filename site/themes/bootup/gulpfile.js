1bda356543c0311b99a9e82ce8d9489cc58de6c4
Michiel Zoer <studio@vlijmscherp.com>
2018-06-06 13:39:13 +0200

    added inital theme files

---
+++ site/themes/bootup/gulpfile.js
@@ -0,0 +1,117 @@
+
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    header  = require('gulp-header'),
+    rename = require('gulp-rename'),
+    cssnano = require('gulp-cssnano'),
+    concat = require('gulp-concat'),
+    package = require('./package.json'),
+    notify  = require('gulp-notify');
+
+var config = {
+        bootstrapDir: './node_modules/bootstrap',
+        publicDir: './public/site/themes/bootup',
+        srcDir: './public/site/themes/bootup/src',
+    };
+
+var banner = [
+  '/*!\n' +
+  ' * <%= package.name %>\n' +
+  ' * <%= package.title %>\n' +
+  ' * <%= package.url %>\n' +
+  ' * @author <%= package.author %>\n' +
+  ' * @version <%= package.version %>\n' +
+  ' * Copyright ' + new Date().getFullYear() + '. <%= package.license %> licensed.\n' +
+  ' */',
+  '\n'
+].join('');
+
+
+
+// array of js files to simply copy to destination
+var jsfiles = [
+    //'./node_modules/slick-carousel/slick/slick.js'
+  ];
+
+gulp.task('jscopy', function () {
+    gulp.src(jsfiles)
+    .pipe(gulp.dest(config.publicDir+'/js'));
+});
+
+
+// array of sass/src files to simply copy to destination
+// run this command only once, otherwise modifications will be overwritten!
+// $ gulp csscopy
+var cssfiles = [
+    './node_modules/slick-carousel/slick/slick-theme.scss',
+    './node_modules/slick-carousel/slick/slick.scss'
+  ];
+
+gulp.task('csscopy', function () {
+    gulp.src(cssfiles)
+    .pipe(gulp.dest(config.srcDir+'/scss/vendor'));
+});
+
+
+gulp.task('bowerscripts', function() {
+  return gulp.src([
+      'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
+      'node_modules/slick-carousel/slick/slick.js'
+    ])
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest(config.publicDir+'/js'))
+    .pipe(notify({
+        message: "Vendor JS files concatenated",
+    }));
+});
+
+
+
+gulp.task('css', function () {
+    return gulp.src(config.srcDir+'/scss/style.scss')
+    .pipe(sass({
+        includePaths: [
+            config.bootstrapDir + '/scss'
+        ],
+    }).on('error', sass.logError))
+    .pipe(autoprefixer('last 4 version'))
+    .pipe(gulp.dest(config.publicDir+'/css'))
+    .pipe(cssnano())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(header(banner, { package : package }))
+    .pipe(gulp.dest(config.publicDir+'/css'))
+    .pipe(browserSync.reload({stream:true}))
+    .pipe(notify({
+        message: "CSS Re-Generated",
+    }));
+});
+
+// gulp.task('fonts', function() {
+//     return gulp.src(config.bootstrapDir + '/assets/fonts/**/*')
+//     .pipe(gulp.dest(config.publicDir + '/assets/fonts'));
+// });
+
+gulp.task('js',function(){
+  gulp.src('src/js/*.js')
+    .pipe(header(banner, { package : package }))
+    .pipe(concat('site_scripts.js'))
+    .pipe(gulp.dest(config.publicDir+'/js'))
+    .pipe(uglify())
+    .pipe(header(banner, { package : package }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(config.publicDir+'/js'))
+    .pipe(browserSync.reload({stream:true, once: true}))
+    .pipe(notify({
+        message: "Js files concatenated",
+    }));    
+});
+
+gulp.task('default', ['bowerscripts','css', 'js'], function () {
+    gulp.watch("src/scss/*/*.scss", ['css']);
+    gulp.watch("src/js/*.js", ['js']);
+});
